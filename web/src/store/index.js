4bcce8597dfc11815ca5c83dc65a7718eaafe911
Gabriel Raniere <gabriel.raniere@tenda.digital>
2020-10-15 17:55:39 -0300

    add missing try/catch

--- web/src/store/index.js
+++ web/src/store/index.js
@@ -7,18 +7,29 @@ import persistedState from 'vuex-persistedstate';
 Vue.use(Vuex)
 
 export default function createStore({ communityId, userId }) {
-  const options = {
-    strict: true,
-    plugins: [
-      persistedState({
-        key: `@${communityId}:${userId}-layers-financeiro`,
-      }),
-    ],
-    modules: {
-      layers,
-      payables,
-    },
-  };
+  let options
+  try {
+    options = {
+      strict: true,
+      plugins: [
+        persistedState({
+          key: `@${communityId}:${userId}-layers-financeiro`,
+        }),
+      ],
+      modules: {
+        layers,
+        payables,
+      },
+    };
+  } catch {
+    options = {
+      strict: true,
+      modules: {
+        layers,
+        payables,
+      },
+    };
+  }
 
   return new Vuex.Store(options);
 }
