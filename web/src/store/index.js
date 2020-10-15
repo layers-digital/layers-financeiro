e6235eac4d255d50baf9a78c21de873ca6637436
Gabriel Raniere <gabriel.raniere@tenda.digital>
2020-10-15 16:59:18 -0300

    make store syncronous

--- web/src/store/index.js
+++ web/src/store/index.js
@@ -3,35 +3,22 @@ import Vuex from 'vuex'
 import payables from './payables'
 import layers from './layers'
 import persistedState from 'vuex-persistedstate';
-import getQueryVariable from '@/helpers/getQueryVariable'
 
 Vue.use(Vuex)
 
-const community = getQueryVariable('community')
-
-let options
-try {
-  options = 
-  {
+export default function createStore({ communityId, userId }) {
+  const options = {
     strict: true,
     plugins: [
       persistedState({
-        key: `${community}-layers-financeiro`
-      })
+        key: `@${communityId}:${userId}-layers-financeiro`,
+      }),
     ],
     modules: {
       layers,
       payables,
     },
-  }
-} catch {
-  options =  {
-    strict: true,
-    modules: {
-      layers,
-      payables,
-    },
-  }
-}
+  };
 
-export default new Vuex.Store(options)
+  return new Vuex.Store(options);
+}
