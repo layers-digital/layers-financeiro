82168135b46270b5488b0a1afd1885330a98e876
CarlosZoft <carlosk2gunn@hotmail.com>
2022-10-21 18:03:06 -0300

    🐛 fix : added verification when has invalid cents paid
    Co-authored-by: Luiz Eduardo <luiz.gotardo@layers.education>

--- web/src/components/PayableDetailCard.vue
+++ web/src/components/PayableDetailCard.vue
@@ -18,7 +18,11 @@
       </span>
     </div>
     <div class="ls-row ls-no-gutters">
-      <div class="ls-col mr-2 amount" :class="amountColor">
+      <div
+        class="ls-col mr-2 amount"
+        :class="amountColor"
+        v-if="payable.centsPaid && payable.centsPaid.toString() != 0"
+      >
         <span v-if="payable.status == 'paid'" style="white-space: nowrap">
           {{ payable.amountPaid }}
         </span>
