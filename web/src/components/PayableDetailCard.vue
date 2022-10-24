245024ace10272492cd6530beb97c6351b48277f
CarlosZoft <carlosk2gunn@hotmail.com>
2022-10-24 16:55:38 -0300

    🐛 fix : by default 0 == false

--- web/src/components/PayableDetailCard.vue
+++ web/src/components/PayableDetailCard.vue
@@ -18,7 +18,7 @@
       </span>
     </div>
     <div class="ls-row ls-no-gutters">
-      <div class="ls-col mr-2 amount" :class="amountColor" v-if="payable.centsPaid && payable.centsPaid != 0">
+      <div class="ls-col mr-2 amount" :class="amountColor" v-if="payable.centsPaid">
         <span v-if="payable.status == 'paid'" style="white-space: nowrap">
           {{ payable.amountPaid }}
         </span>
