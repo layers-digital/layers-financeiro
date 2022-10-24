b1492a194bc9022e5844956e845a334d67043f6b
CarlosZoft <carlosk2gunn@hotmail.com>
2022-10-24 19:15:46 -0300

    🐛 fix : go back button

--- web/src/components/SimpleAppBar.vue
+++ web/src/components/SimpleAppBar.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="simple-bar-container ls-row ls-no-gutters ls-align-items-center p-2">
-    <div class="ls-col ls-flex-grow-0 mr-1" style="cursor: pointer" @click="$router.go(-1)">
+    <div class="ls-col ls-flex-grow-0 mr-1" style="cursor: pointer" @click="$router.back()">
       <img src="../assets/arrow-left.svg" height="32" width="32" style="margin-bottom: -4px" />
     </div>
     <span class="title">Detalhe da cobrança</span>
