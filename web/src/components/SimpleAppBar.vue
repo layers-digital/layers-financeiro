70263f98745cc4cae4213556cd4c4cb5b8d5f3b3
CarlosZoft <carlosk2gunn@hotmail.com>
2022-10-25 15:12:45 -0300

    🐛 fix : taking responsibility away from window to escape the app stack router

--- web/src/components/SimpleAppBar.vue
+++ web/src/components/SimpleAppBar.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="simple-bar-container ls-row ls-no-gutters ls-align-items-center p-2">
-    <div class="ls-col ls-flex-grow-0 mr-1" style="cursor: pointer" @click="$router.go(-1)">
+    <div class="ls-col ls-flex-grow-0 mr-1" style="cursor: pointer" @click="goBack">
       <img src="../assets/arrow-left.svg" height="32" width="32" style="margin-bottom: -4px" />
     </div>
     <span class="title">Detalhe da cobrança</span>
@@ -10,6 +10,11 @@
 <script>
 export default {
   name: 'SimpleAppBar',
+  methods: {
+    goBack() {
+      this.$router.push({ name: 'payables.group', params: { groupId: this.$route.params.payable.groupId } });
+    },
+  },
 };
 </script>
 
