4ed17b2a2092c799dd00224129d2ea22a96ab1d6
Tulio  Caviquioli <tulio.cardoso@layers.education>
2023-03-03 17:34:59 -0300

    let to const

--- web/src/helpers/currencyFormatter.js
+++ web/src/helpers/currencyFormatter.js
@@ -1,10 +1,10 @@
 export default function currencyFormatter(value, currency, locale) {
-  const integerValue = value / 100;
+  const realValue = value / 100;
   if (!currency) {
     currency = 'BRL';
   }
   return new Intl.NumberFormat(locale, {
     style: 'currency',
     currency,
-  }).format(integerValue);
+  }).format(realValue);
 }
